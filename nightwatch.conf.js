7b87a20fa538ed76d8fcce51ea4b3e0eb2c504e8
Yifeng Wu <f15gdsy@gmail.com>
2017-03-15 16:54:07 +0800

    update nightwatch

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,16 +2,16 @@ module.exports = {
   'src_folders': ['test/e2e'],
   'output_folder': 'test/e2e/reports',
 
-  'selenium': {
-    'start_process': true,
-    'server_path': require('selenium-server').path,
-    'host': '127.0.0.1',
-    'port': 4444,
-    'cli_args': {
-      'webdriver.chrome.driver': require('chromedriver').path
-      , 'webdriver.gecko.driver': 'node_modules/geckodriver/geckodriver'
-    }
-  },
+  // 'selenium': {
+  //   'start_process': true,
+  //   'server_path': require('selenium-server').path,
+  //   'host': '127.0.0.1',
+  //   'port': 4444,
+  //   'cli_args': {
+  //     'webdriver.chrome.driver': require('chromedriver').path
+  //     , 'webdriver.gecko.driver': 'node_modules/geckodriver/geckodriver'
+  //   }
+  // },
 
   'test_settings': {
     'default': {
